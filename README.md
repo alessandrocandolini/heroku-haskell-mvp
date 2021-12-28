74032d11c560d480d1771fdb7fb4fd00d444f99e
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-12-28 00:29:43 +0000

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# heroku-haskell-mvp
+
+## How to build and run locally
+
+The project uses the [Haskell tool stack](https://docs.haskellstack.org/en/stable/README/).
+
+Assuming `stack` is installed in the system, the project can be build by running
+```
+stack build
+```
+To build and also run the tests, run
+```
+stack test
+```
+which is equivalent to
+```
+stack build --test
+```
+To run the executable,
+```
+stack exec heroku-haskell-mvp-exe
+```
+For a faster feedback loop,
+```
+stack test --fast --file-watch
+```
+To run `ghci` (with a version compatible with the resolver) run
+```
+stack ghci
+```
+For more information, refer to the `stack` official docs.
---
+++ src/Lib.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Lib where
+
+import Data.Aeson (ToJSON)
+import Data.Data (Proxy (Proxy))
+import GHC.Generics (Generic)
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+import Servant (Get, Server, (:>))
+import Servant.API (JSON)
+import Servant.Server (serve)
+
+newtype StatusResponse = StatusResponse
+  { status :: String
+  }
+  deriving (Generic, Eq, Show, Ord)
+
+instance ToJSON StatusResponse
+
+type StatusEndpoint = "status" :> Get '[JSON] StatusResponse
+
+type API = StatusEndpoint 
+
+api :: Proxy API
+api = Proxy
+
+app :: Application
+app = serve api server
+
+startServer :: IO ()
+startServer = run 8080 app
+
+server :: Server API
+server = pure ok where 
+   ok = StatusResponse "ok"
