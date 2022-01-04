438a047f6e50eda8b455226a2b1c0985ad0aa46b
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-01-04 03:08:26 +0000

    port configurable

--- src/Server.hs
+++ src/Server.hs
@@ -6,12 +6,15 @@ module Server where
 
 import Data.Aeson (ToJSON)
 import Data.Data (Proxy (Proxy))
+import Data.Maybe (fromMaybe)
 import GHC.Generics (Generic)
 import Network.Wai (Application)
-import Network.Wai.Handler.Warp (run)
+import Network.Wai.Handler.Warp (Settings, defaultSettings, run, runSettings, setHost, setPort, setTimeout)
 import Servant (Get, Server, (:>))
 import Servant.API (JSON)
 import Servant.Server (serve)
+import System.Environment (lookupEnv)
+import Text.Read (readMaybe)
 
 newtype StatusResponse = StatusResponse
   { status :: String
@@ -30,8 +33,17 @@ api = Proxy
 app :: Application
 app = serve api server
 
+settings :: IO Settings
+settings = buildSettings . parsePort <$> lookupEnv "PORT"
+
+parsePort :: Maybe String -> Int
+parsePort = fromMaybe 8080 . (=<<) readMaybe
+
+buildSettings :: Int -> Settings
+buildSettings port = setTimeout 30 $ setPort port $ setHost "0.0.0.0" defaultSettings
+
 startServer :: IO ()
-startServer = run 8080 app
+startServer = settings >>= \s -> runSettings s app
 
 server :: Server API
 server = pure ok
